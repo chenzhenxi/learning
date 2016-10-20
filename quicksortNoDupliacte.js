b57ebf645964ec344ee268e121e641e7f0a8ef1c
Eric Chen <zhenxi.chen@gmail.com>
2016-10-20 15:29:34 +1300

    Update quicksortNoDupliacte.js

--- quicksortNoDupliacte.js
+++ quicksortNoDupliacte.js
@@ -3,14 +3,15 @@ function quicksortNoDupliacte(array, lo = 0, hi = array.length - 1) {
   
   let i = lo, j = hi + 1;
   
-  while (i < j) {
+  while (true) {
     while(i < hi && array[++i] < array[lo]) {}
     while(lo < j && array[--j] > array[lo]) {}
-    
-    if (i < j) {
-      [array[i], array[j]] = [array[j], array[i]];
+    if (i >= j) {
+      break;
     }
+    [array[i], array[j]] = [array[j], array[i]];
   }
+  
   [array[lo], array[j]] = [array[j], array[lo]];  
   quicksortNoDupliacte(array, lo, j - 1);
   quicksortNoDupliacte(array, j + 1, hi);

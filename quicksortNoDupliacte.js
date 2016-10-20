b19877bc569b2c6b32535fb2d5e84a3c56726119
Eric Chen <zhenxi.chen@gmail.com>
2016-10-20 16:14:23 +1300

    Update quicksortNoDupliacte.js

--- quicksortNoDupliacte.js
+++ quicksortNoDupliacte.js
@@ -1,7 +1,8 @@
 function quicksortNoDupliacte(array, lo = 0, hi = array.length - 1) {
   if (lo >= hi) return array;
-  
+    
   let i = lo, j = hi + 1;
+  shuffle(array, i, j);
   
   while (true) {
     while(i < hi && array[++i] < array[lo]) {}
@@ -17,4 +18,12 @@ function quicksortNoDupliacte(array, lo = 0, hi = array.length - 1) {
   quicksortNoDupliacte(array, j + 1, hi);
   
   return array;
+}
+
+function shuffle(a, lo = 0, hi = a.length) {
+	let diff = hi - lo;
+    for (let i = hi; i > lo; i--) {
+        let j = lo + parseInt(Math.random() * diff, 10);
+        [a[i - 1], a[j]] = [a[j], a[i - 1]];
+    }
 }

921d109f92962fc0eecb502ce36d544c3b3481d5
Eric Chen <zhenxi.chen@gmail.com>
2016-11-03 10:41:52 +1300

    Update bst.js

--- bst.js
+++ bst.js
@@ -5,3 +5,26 @@ function Node ({ key, val = null, left = null, right = null }) {
   this.right = right;
 }
 
+function BST() {
+  this.root = null;
+}
+
+BST.prototype.put = function(key, val) {
+};
+
+BST.prototype.get = function(key) {
+  let next = root;
+  while (next) {
+    if (next.key === key) {
+      return next.val;
+    } else if (next.key > key) {
+      next = next.left;
+    } else {
+      next = next.right;
+    }
+  }
+  return null;
+};
+
+BST.prototype.delete = function(key) {
+}
